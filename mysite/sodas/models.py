2b9ec915496ed69c4d25a475929937721d1aad77
Xavier A Leon <demolitiongamer256@gmail.com>
2024-04-25 23:43:39 -0600

    turning in final

--- mysite/sodas/models.py
+++ mysite/sodas/models.py
@@ -8,6 +8,12 @@ class SodaCombo(models.Model):
 
     name = models.CharField(max_length=200)
     soda = models.CharField(max_length=100)
-    flavor1 = models.CharField(max_length=100)
+    # All are optional below this point
+    # There are combos with no syrups but do have purees
+    flavor1 = models.CharField(max_length=100, null=True, blank=True)
     flavor2 = models.CharField(max_length=100, null=True, blank=True)
     flavor3 = models.CharField(max_length=100, null=True, blank=True)
+    flavor4 = models.CharField(max_length=100, null=True, blank=True)
+    addon1 = models.CharField(max_length=100, null=True, blank=True)
+    addon2 = models.CharField(max_length=100, null=True, blank=True)
+    splash = models.CharField(max_length=100, null=True, blank=True)
--- mysite/sodas/views.py
+++ mysite/sodas/views.py
@@ -7,44 +7,48 @@ def filterswig(request):
     soda_object = SodaCombo.objects.all()
     soda_base = request.GET.get('soda')
 
+    # Filters through the soda bases
     if soda_base:
         if soda_base == "drpepper":
-            soda_object = filter_drpepper(soda_object)
+            soda_object = base_drpepper(soda_object)
         elif soda_base == "coke":
-            soda_object = filter_coke(soda_object)
+            soda_object = base_coke(soda_object)
         elif soda_base == "mtndew":
-            soda_object = filter_mtndew(soda_object)
+            soda_object = base_mtndew(soda_object)
         elif soda_base == "sprite":
-            soda_object = filter_sprite(soda_object)
+            soda_object = base_sprite(soda_object)
         elif soda_base == "lemonade":
-            soda_object = filter_lemonade(soda_object)
+            soda_object = base_lemonade(soda_object)
         elif soda_base == "rootbeer":
-            soda_object = filter_rootbeer(soda_object)
+            soda_object = base_rootbeer(soda_object)
         elif soda_base == "fresca":
-            soda_object = filter_fresca(soda_object)
+            soda_object = base_fresca(soda_object)
+
+    # Filters through the soda flavors
+    soda_flavor = request.GET.get('flavor')
 
     return render(request, 'sodas/filter.html', {'soda_object': soda_object})
 
 # All the filter functions that filter off of soda base
-def filter_drpepper(soda_object):
+def base_drpepper(soda_object):
     return soda_object.filter(soda__icontains="Dr. Pepper")
 
-def filter_coke(soda_object):
+def base_coke(soda_object):
     return (soda_object.filter(soda__icontains="Coca Cola")
             | soda_object.filter(soda__icontains="Diet Coke")
             | soda_object.filter(soda__icontains="Coke Zero"))
 
-def filter_mtndew(soda_object):
+def base_mtndew(soda_object):
     return soda_object.filter(soda__icontains="Mountain Dew")
 
-def filter_sprite(soda_object):
+def base_sprite(soda_object):
     return soda_object.filter(soda__icontains="Sprite")
 
-def filter_lemonade(soda_object):
+def base_lemonade(soda_object):
     return soda_object.filter(soda__icontains="Lemonade")
 
-def filter_rootbeer(soda_object):
+def base_rootbeer(soda_object):
     return soda_object.filter(soda__icontains="Root Beer")
 
-def filter_fresca(soda_object):
+def base_fresca(soda_object):
     return soda_object.filter(soda__icontains="Fresca")
